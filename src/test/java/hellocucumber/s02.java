02d73389fbd38bb1194fd7294ddce76ee9cf49db
Chris Li <chris1e686@gmail.com>
2020-11-12 15:15:43 -0500

    create story 1

--- src/test/java/hellocucumber/s02.java
+++ src/test/java/hellocucumber/s02.java
@@ -1,5 +1,6 @@
 package hellocucumber;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
@@ -26,5 +27,78 @@ public class s02 extends ApiTest {
                 get("/categories").
                 then().
                 statusCode(200);
+    }
+
+
+
+
+    @Given("there is a valid endpoint")
+    public void there_is_a_valid_endpoint() {
+        // Write code here that turns the phrase above into concrete actions
+        setup();
+    }
+
+    @Given("there is a task with no priority category")
+    public void there_is_a_task_with_no_priority_category() {
+        RestAssured.baseURI = "http://localhost:4567";
+        when().
+                get("/categories").
+                then().
+                statusCode(200);
+    }
+
+    @Given("there are priority categories as HIGH, MEDIUM, and LOW")
+    public void there_are_priority_categories_as_high_medium_and_low() {
+        RestAssured.baseURI = "http://localhost:4567";
+        when().
+                get("/categories").
+                then().
+                statusCode(200);
+    }
+    @When("I change the category to HIGH")
+    public void i_change_the_category_to_high() {
+        RestAssured.baseURI = "http://localhost:4567";
+        when().
+                get("/categories").
+                then().
+                statusCode(200);
+    }
+    @When("select HIGH")
+    public void select_high() {
+        RestAssured.baseURI = "http://localhost:4567";
+        when().
+                get("/categories").
+                then().
+                statusCode(200);
+    }
+    @Then("The task should be categorized as HIGH")
+    public void the_task_should_be_categorized_as_high() {
+        RestAssured.baseURI = "http://localhost:4567";
+        when().
+                get("/categories").
+                then().
+                statusCode(200);
+    }
+
+    @Given("there is a task with HIGH priority")
+    public void there_is_a_task_with_high_priority() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("I change the category to LOW")
+    public void i_change_the_category_to_low() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("The task should be categorized as LOW")
+    public void the_task_should_be_categorized_as_low() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("close server")
+    public void close_server() {
+        // Write code here that turns the phrase above into concrete actions
+        shutdown();
     }
 }
--- target/test-classes/hellocucumber/s01-categorize-task.feature
+++ target/test-classes/hellocucumber/s01-categorize-task.feature
@@ -1,18 +1,30 @@
-Feature: Categorize priority
-  User wants to categorize tasks a HIGH, MEDIUM or LOW
+Feature: Categorize A Task
 
-  Scenario: There is a task that is not categorized
-    Given there is a valid endpoint
-    Given there is a task with no priority category
-    And there are priority categories as HIGH, MEDIUM, and LOW
-    When I change the category to HIGH
-    Then The task should be categorized as HIGH
-    And close server
+  As a student, I categorize tasks as HIGH, MEDIUM or LOW priority, so I can better manage my time.
 
-  Scenario: There is a task that is categorized (alternative flow)
-    Given there is a valid endpoint
-    Given there is a task with HIGH priority
-    And there are priority categories as HIGH, MEDIUM, and LOW
-    When I change the category to LOW
-    Then The task should be categorized as LOW
-    And close server
+  Background:
+    Given The server is running
+
+  Scenario: Categorize a Task without Priority Level (Normal Flow)
+
+    Given There are HIGH, MEDIUM and LOW priority categories
+    And A Todo named 'Assignment1' has no priority category
+    When I request to change Todo 'Assignment1' to HIGH priority category
+    Then I should receive a confirmation message
+    And I shutdown the server
+
+  Scenario: Categorize a Task to Another Priority Level (Alternate Flow)
+
+    Given There are HIGH, MEDIUM and LOW priority categories
+    And A Todo named 'Assignment1' is in LOW priority category
+    When I request to change Todo 'Assignment1' to HIGH priority category
+    Then I should receive a confirmation message
+    And I shutdown the server
+
+  Scenario: Categorize a Task that does not exist (Error Flow)
+
+    Given There are HIGH, MEDIUM and LOW priority categories
+    And There is no Todo named 'Assignment1' in the system
+    When I request to change Todo 'Assignment1' to HIGH priority category
+    Then I should receive an error message
+    And I shutdown the server
